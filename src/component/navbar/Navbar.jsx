6951b22af61cad10f6c3e187939a7c9e96447f7f
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-19 17:42:35 +0530

    Edit ui in Navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import Links from "./links/Links";
+import Image from "next/image";
 import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.logo}>Logo </div>
+        <div className={styles.imgContainer}>
+          <Image src="/Logo.png" alt="Logo" fill className={styles.img} />
+        </div>
         <div>
           <Links />
         </div>
