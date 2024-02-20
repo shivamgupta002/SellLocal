7ffcea672025ba9d7952ef1375329bd3eb40b9d1
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-20 14:42:13 +0530

    Not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,24 @@
-const notFound = () => {
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./customComponent.module.css";
+const NotFound = () => {
   return (
     <>
-      <div>Page Not found</div>
+      <div className={styles.loadingDiv}>
+        <Image
+          src="/NotFound.svg"
+          alt="loading.."
+          fill
+          className={styles.loading}
+        />
+      </div>
+      <div className={styles.returnLink}>
+        <h3>Return to Home page</h3>
+        <Link href="/" className={styles.link}>Click here</Link>
+      </div>
     </>
   );
 };
 
-export default notFound;
+export default NotFound;
