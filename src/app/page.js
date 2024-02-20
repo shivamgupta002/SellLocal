b612e47c4f32654018ef54529da77bc0578bee49
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-20 17:01:46 +0530

    Add Home Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroSection from "../component/heroSection/HeroSection"
+// import Navbar from "../component/navbar/Navbar.jsx";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     Hello Home page
+    <HeroSection/>
     </main>
   );
 }
---
+++ src/component/heroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import styles from "./heroSection.module.css";
+const HeroSection = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        Hello from Hero Section
+      </div>
+    </>
+  );
+};
+export default HeroSection;
