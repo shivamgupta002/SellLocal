79c5aa70918a94c98c7d696c386cb148fcfeb2f3
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-20 16:18:33 +0530

    Navbar Ui edit

--- src/component/navbar/links/Links.jsx
+++ src/component/navbar/links/Links.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
 import Image from "next/image";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from '@mui/icons-material/Menu';
 const links = [
   {
-    title: "HomePage",
+    title: "Home",
     path: "/",
   },
   {
@@ -13,8 +15,8 @@ const links = [
     path: "/about",
   },
   {
-    title: "Contact",
-    path: "/contact",
+    title: "Become a Seller",
+    path: "/seller",
   },
   {
     title: "Carrier",
@@ -33,6 +35,10 @@ const Links = () => {
           {links.map((link) => (
             <NavLink item={link} key={link.title} />
           ))}
+          <div className={styles.divLink}>
+            <ShoppingCartIcon />
+            <NavLink item={{ title: "Cart", path: "/cart" }} />
+          </div>
           {session ? (
             <>
               {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
@@ -42,12 +48,17 @@ const Links = () => {
             <NavLink item={{ title: "login", path: "/login" }} />
           )}
         </div>
-        <Image src="/menu.png" alt="hamburger" width={30} height={30} onClick={() => setOpen((prev) => !prev)} className={styles.menuButton} />
+        <MenuIcon onClick={() => setOpen((prev) => !prev)}
+          className={styles.menuButton} />
         {open && (
           <div className={styles.mobileLinks}>
             {links.map((link) => (
               <NavLink item={link} key={link.title} />
             ))}
+            <div className={styles.divLink}>
+              <ShoppingCartIcon />
+              <NavLink item={{ title: "Cart", path: "/cart" }} />
+            </div>
           </div>
         )}
       </div>
