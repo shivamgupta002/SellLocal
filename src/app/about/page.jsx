ac3037429793c8515b564f75e731e9160729a6a1
shivamgupta002 <shivamgupta992909@gmail.com>
2024-02-27 14:16:10 +0530

    about page add

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,54 @@
+// import Image from "next/image";
+// import styles from "./about.module.css";
+// const About = () => {
+//   return (
+//     <>
+//       <div className={styles.container}>
+//         <div className={styles.textContainer}>
+//           <h2>Our Story</h2>
+// <p>
+//   Inspired by the success of platforms like Amazon, we are thrilled to
+//   introduce Sell local, a revolutionary project dedicated to enhancing
+//   local markets and making internet selling and buying accessible to
+//   alt. Our mission is simple to connect local sellers with online
+//   buyers while ensuring the price affordability for a wider reach.
+// </p>
+// <p>
+//   Imagine accessing all products directly from local vendors in your
+//   city, all through a user-friendly app to your door step.With Sell
+//   Local, you con browse a diverse range of items, from everyday
+//   essentials to handcrafted treasures, at comparatively low prices.
+//   Our platform empowers entrepreneurs ond small business to show case
+//   their product to a wider audience.
+// </p>
+// <p>
+//   We are excited to present SellLocal a transformative initiative that
+//   aims at enhancing local markets and making internet selling more
+//   inclusive, following the success of platforms like Amazon. Our goal
+//   is simple enabling inexpensive prices that can reach many people, by
+//   bringing together local seller with online buyers.
+// </p>
+//         </div>
+//         <div className={styles.imgContainer}>
+//           <div className={styles.heroSection}>
+//             <Image
+//               src="/Image/about1.jpg"
+//               alt="about"
+//               fill
+//               className={styles.aboutTopImage}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default About;
+
 import Image from "next/image";
 import styles from "./about.module.css";
+
 const About = () => {
   return (
     <>
@@ -34,8 +83,8 @@ const About = () => {
             <Image
               src="/Image/about1.jpg"
               alt="about"
-              fill
-              className={styles.aboutTopImage}
+              layout="fill"
+              className={`${styles.aboutTopImage} ${styles.imageHover}`}
             />
           </div>
         </div>
