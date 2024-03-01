b16748c7e63e65110cadee8b973b75ae294e1797
shivamgupta002 <shivamgupta992909@gmail.com>
2024-03-01 14:33:48 +0530

    edit ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
 import MidSection from "@/component/Home/MidSection";
 import styles from "./page.module.css";
 import Banner from "@/component/Home/Banner";
+const Component = styled(Box)`
+  padding: 10px;
+  background: #f2f2f2;
+`;
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Banner />
-      <MidSection/>
+      <Component>
+        <Banner />
+        <MidSection />
+      </Component>
     </main>
   );
 }
