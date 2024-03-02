2418b8934421c455ee9962dc9eb1423f0c4e4c79
shivamgupta002 <shivamgupta992909@gmail.com>
2024-03-02 15:26:18 +0530

    add styled tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+"use client";
 import MidSection from "@/component/Home/MidSection";
 import styles from "./page.module.css";
 import Banner from "@/component/Home/Banner";
+import { Box, styled } from "@mui/material";
+// import MidSlide from "@/component/Home/MidSlide";
+// import Slide from "@/component/Home/Slide";
 const Component = styled(Box)`
   padding: 10px;
   background: #f2f2f2;
@@ -10,6 +14,8 @@ export default function Home() {
     <main className={styles.main}>
       <Component>
         <Banner />
+        {/* <MidSlide products={products} title="Deal of the Day" timer={true} /> */}
+        {/* <Slide products={products} title="Discounts for You" timer={false} /> */}
         <MidSection />
       </Component>
     </main>
